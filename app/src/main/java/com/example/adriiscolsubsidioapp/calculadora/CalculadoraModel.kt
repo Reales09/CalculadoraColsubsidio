74ac58fb6a8fa81fcf72f197cc4fe1aa456246cf
Reales <rclarence123@gmail.com>
2021-05-21 22:57:20 -0500

    Encapsulate the LiveData

--- app/src/main/java/com/example/adriiscolsubsidioapp/calculadora/CalculadoraModel.kt
+++ app/src/main/java/com/example/adriiscolsubsidioapp/calculadora/CalculadoraModel.kt
@@ -1,24 +1,40 @@
 package com.example.adriiscolsubsidioapp.calculadora
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class CalculadoraModel : ViewModel() {
 
-
     //variables movidas del fragment
-    var money = MutableLiveData<Long>()
-    var numPerso= MutableLiveData<Long>()
-    var totalValTodos=MutableLiveData<Long>()
-    var numHijos=MutableLiveData<Long>()
-    var totaldefi= MutableLiveData<Long>()
+    private val _money = MutableLiveData<Long>()
+    val money: LiveData<Long>
+    get() = _money
+
+    private val _numPerso= MutableLiveData<Long>()
+    val numPerso:LiveData<Long>
+    get() = _numPerso
+
+
+    private val _totalValTodos=MutableLiveData<Long>()
+    val totalValTodos: LiveData<Long>
+    get() = _totalValTodos
+
+    private val _numHijos=MutableLiveData<Long>()
+    val numHijos: LiveData<Long>
+    get()=_numHijos
+
+    private val _totaldefi= MutableLiveData<Long>()
+    val totaldefi: LiveData<Long>
+    get() = _totaldefi
+
 
     init {
-        money.value=0
-        numPerso.value=0
-        totalValTodos.value=0
-        numHijos.value=0
-        totaldefi.value=0
+        _money.value=0
+        _numPerso.value=0
+        _totalValTodos.value=0
+        _numHijos.value=0
+        _totaldefi.value=0
 
     }
 
